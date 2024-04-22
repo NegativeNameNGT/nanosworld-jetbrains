cc6ea74dc721c84ac8fe3f3af3f1956d3be5d6fd
NegativeName <negativenamengt@gmail.com>
2024-04-22 21:09:57 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -371,8 +371,8 @@ function ${cls.name}${
 		cls.static_properties.forEach((prop, index) => {
 			static_fields += `${prop.name} = ${prop.value}`;
 
-			if (index < cls.static_properties.length - 1) {
-				static_fields += ", ";
+			if (cls.static_properties !== undefined && index !== cls.static_properties.length - 1)
+				static_fields += ",";
 			}
 		});
 		static_fields += " ";
